5f9302c7de68d4aa3617c03cfb82ca88f818c918
slaveeks <chernyshev2001@icloud.com>
2022-01-14 19:07:56 +0300

    Added .env file

--- notify/main.py
+++ notify/main.py
@@ -1,5 +1,5 @@
 from sdk.codexbot_sdk import CodexBot
-from config import APPLICATION_TOKEN, APPLICATION_NAME, DB, URL, SERVER, RABBITMQ
+from settings import APPLICATION_TOKEN, APPLICATION_NAME, DB, SERVER, RABBITMQ
 
 from commands.help import CommandHelp
 from commands.start import CommandStart
@@ -10,7 +10,7 @@ class Notify:
 
     def __init__(self):
 
-        self.sdk = CodexBot(APPLICATION_NAME, SERVER['host'], SERVER['port'], db_config=DB, rabbitmq_host=RABBITMQ['host'], token=APPLICATION_TOKEN)
+        self.sdk = CodexBot(APPLICATION_NAME, SERVER['host'], SERVER['port'], db_config=DB, rabbitmq_host=RABBITMQ, token=APPLICATION_TOKEN)
 
         self.sdk.log("Notify module initialized")
 
